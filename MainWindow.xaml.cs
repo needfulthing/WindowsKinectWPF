b0bc206e8416326acd18892aa15d82a1d48ad35c
Maximilian Laurenz <max@daheimweh.de>
2020-02-28 14:42:05 +0100

    Fix wrong solution

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,14 +1,22 @@
-﻿using Microsoft.Kinect;
-using Microsoft.Kinect.Toolkit;
-using System;
-using System.Globalization;
-using System.Linq;
-using System.Windows;
-using System.Windows.Media;
-using System.IO.Ports;
+﻿//------------------------------------------------------------------------------
+// <copyright file="MainWindow.xaml.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
 
-namespace KinectSDK18
+namespace Microsoft.Samples.Kinect.DepthBasics
 {
+	using System;
+	using System.Globalization;
+	using System.IO;
+	using System.Windows;
+	using System.Windows.Media;
+	using System.Windows.Media.Imaging;
+	using Microsoft.Kinect;
+	using Emgu.CV;
+	using System.Runtime.InteropServices;
+	using Emgu.CV.UI;
+
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
@@ -19,119 +27,187 @@ namespace KinectSDK18
 		/// </summary>
 		private KinectSensor sensor;
 
+		/// <summary>
+		/// Bitmap that will hold color information
+		/// </summary>
+		private WriteableBitmap colorBitmap;
+
+		/// <summary>
+		/// Intermediate storage for the depth data received from the camera
+		/// </summary>
+		private DepthImagePixel[] depthPixels;
+
+		/// <summary>
+		/// Intermediate storage for the depth data converted to color
+		/// </summary>
+		private byte[] colorPixels;
+
+		private Emgu.CV.BackgroundSubtractorKNN backSubDepth, backSubRgb;
+
+		/// <summary>
+		/// Initializes a new instance of the MainWindow class.
+		/// </summary>
 		public MainWindow()
 		{
 			InitializeComponent();
-			Loaded += MainWindowLoaded;
 		}
 
-		private void MainWindowLoaded(object sender, RoutedEventArgs e)
+		/// <summary>
+		/// Execute startup tasks
+		/// </summary>
+		/// <param name="sender">object sending the event</param>
+		/// <param name="e">event arguments</param>
+		private void WindowLoaded(object sender, RoutedEventArgs e)
 		{
-			var sensorStatus = new KinectSensorChooser();
-
-			sensorStatus.KinectChanged += KinectSensorChooserKinectChanged;
-
-			kinectChooser.KinectSensorChooser = sensorStatus;
-			sensorStatus.Start();
-
-		}
-
-		private void KinectSensorChooserKinectChanged(object sender, KinectChangedEventArgs e)
-		{
-
-			if (sensor != null)
-				sensor.SkeletonFrameReady -= KinectSkeletonFrameReady;
-
-			sensor = e.NewSensor;
-
-			if (sensor == null)
-				return;
-
-			switch (Convert.ToString(e.NewSensor.Status))
+			// Look through all sensors and start the first connected one.
+			// This requires that a Kinect is connected at the time of app startup.
+			// To make your app robust against plug/unplug, 
+			// it is recommended to use KinectSensorChooser provided in Microsoft.Kinect.Toolkit (See components in Toolkit Browser).
+			foreach (var potentialSensor in KinectSensor.KinectSensors)
 			{
-				case "Connected":
-					KinectStatus.Content = "Connected";
-					break;
-				case "Disconnected":
-					KinectStatus.Content = "Disconnected";
-					break;
-				case "Error":
-					KinectStatus.Content = "Error";
-					break;
-				case "NotReady":
-					KinectStatus.Content = "Not Ready";
-					break;
-				case "NotPowered":
-					KinectStatus.Content = "Not Powered";
-					break;
-				case "Initializing":
-					KinectStatus.Content = "Initialising";
-					break;
-				default:
-					KinectStatus.Content = "Undefined";
-					break;
-			}
-
-			sensor.SkeletonStream.Enable();
-			sensor.SkeletonFrameReady += KinectSkeletonFrameReady;
-
-		}
-
-
-		private void SensorDepthFrameReady(object sender, DepthImageFrameReadyEventArgs e)
-		{
-			throw new NotImplementedException();
-		}
-
-		private void KinectSkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
-		{
-			var skeletons = new Skeleton[0];
-
-			using (var skeletonFrame = e.OpenSkeletonFrame())
-			{
-				if (skeletonFrame != null)
+				if (potentialSensor.Status == KinectStatus.Connected)
 				{
-					skeletons = new Skeleton[skeletonFrame.SkeletonArrayLength];
-					skeletonFrame.CopySkeletonDataTo(skeletons);
+					this.sensor = potentialSensor;
+					break;
 				}
 			}
 
-			if (skeletons.Length == 0)
+			if (null != this.sensor)
 			{
-				return;
-			}
+				// Turn on the depth stream to receive depth frames
+				this.sensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
 
-			var skel = skeletons.FirstOrDefault(x => x.TrackingState == SkeletonTrackingState.Tracked);
-			if (skel == null)
+				// Allocate space to put the depth pixels we'll receive
+				this.depthPixels = new DepthImagePixel[this.sensor.DepthStream.FramePixelDataLength];
+
+				// Allocate space to put the color pixels we'll create
+				this.colorPixels = new byte[this.sensor.DepthStream.FramePixelDataLength * sizeof(int)];
+
+				// This is the bitmap we'll display on-screen
+				this.colorBitmap = new WriteableBitmap(this.sensor.DepthStream.FrameWidth, this.sensor.DepthStream.FrameHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
+
+				// Set the image we display to point to the bitmap where we'll put the image data
+				this.Image.Source = this.colorBitmap;
+
+				// Add an event handler to be called whenever there is new depth frame data
+				this.sensor.DepthFrameReady += this.SensorDepthFrameReady;
+
+				// Start the sensor!
+				try
+				{
+					this.sensor.Start();
+				}
+				catch (IOException)
+				{
+					this.sensor = null;
+				}
+			}
+			Emgu.CV.CvInvoke.NamedWindow("Emgu Window");
+			backSubDepth = new Emgu.CV.BackgroundSubtractorKNN(50, 400, false);
+			//backSubRgb = new Emgu.CV.BackgroundSubtractorKNN(500, 50, false);
+		}
+
+		/// <summary>
+		/// Execute shutdown tasks
+		/// </summary>
+		/// <param name="sender">object sending the event</param>
+		/// <param name="e">event arguments</param>
+		private void WindowClosing(object sender, System.ComponentModel.CancelEventArgs e)
+		{
+			if (null != this.sensor)
 			{
-				return;
+				this.sensor.Stop();
 			}
+		}
 
-			var rightHand = skel.Joints[JointType.WristRight];
-			XValueRight.Text = rightHand.Position.X.ToString(CultureInfo.InvariantCulture);
-			YValueRight.Text = rightHand.Position.Y.ToString(CultureInfo.InvariantCulture);
-			ZValueRight.Text = rightHand.Position.Z.ToString(CultureInfo.InvariantCulture);
-
-			var leftHand = skel.Joints[JointType.WristLeft];
-			XValueLeft.Text = leftHand.Position.X.ToString(CultureInfo.InvariantCulture);
-			YValueLeft.Text = leftHand.Position.Y.ToString(CultureInfo.InvariantCulture);
-			ZValueLeft.Text = leftHand.Position.Z.ToString(CultureInfo.InvariantCulture);
-
-			var centreHip = skel.Joints[JointType.HipCenter];
-
-			if (centreHip.Position.Z - rightHand.Position.Z > 0.3)
+		/// <summary>
+		/// Event handler for Kinect sensor's DepthFrameReady event
+		/// </summary>
+		/// <param name="sender">object sending the event</param>
+		/// <param name="e">event arguments</param>
+		private void SensorDepthFrameReady(object sender, DepthImageFrameReadyEventArgs e)
+		{
+			using (DepthImageFrame depthFrame = e.OpenDepthImageFrame())
 			{
-				RightRaised.Text = "Raised";
+				if (depthFrame != null)
+				{
+					// Copy the pixel data from the image to a temporary array
+					depthFrame.CopyDepthImagePixelDataTo(this.depthPixels);
+
+					// Get the min and max reliable depth for the current frame
+					int minDepth = depthFrame.MinDepth;
+					int maxDepth = depthFrame.MaxDepth;
+
+					// Convert the depth to RGB
+					int colorPixelIndex = 0;
+					for (int i = 0; i < this.depthPixels.Length; ++i)
+					{
+						// Get the depth for this pixel
+						short depth = depthPixels[i].Depth;
+
+						// To convert to a byte, we're discarding the most-significant
+						// rather than least-significant bits.
+						// We're preserving detail, although the intensity will "wrap."
+						// Values outside the reliable depth range are mapped to 0 (black).
+
+						// Note: Using conditionals in this loop could degrade performance.
+						// Consider using a lookup table instead when writing production code.
+						// See the KinectDepthViewer class used by the KinectExplorer sample
+						// for a lookup table example.
+						byte intensity = (byte)(depth >= minDepth && depth <= maxDepth ? depth : 0);
+
+						// Write out blue byte
+						this.colorPixels[colorPixelIndex++] = intensity;
+
+						// Write out green byte
+						this.colorPixels[colorPixelIndex++] = intensity;
+
+						// Write out red byte                        
+						this.colorPixels[colorPixelIndex++] = intensity;
+
+						// We're outputting BGR, the last byte in the 32 bits is unused so skip it
+						// If we were outputting BGRA, we would write alpha here.
+						++colorPixelIndex;
+					}
+
+					var bitmap = BitmapFromWriteableBitmap(colorBitmap);
+					var inputImg = bitmap.ToImage<Emgu.CV.Structure.Bgr, byte>();
+					//CvInvoke.Threshold(inputImg, inputImg, 127, 255, Emgu.CV.CvEnum.ThresholdType.Binary);
+					CvInvoke.Erode(inputImg, inputImg, null, new System.Drawing.Point(-1, -1), 10, Emgu.CV.CvEnum.BorderType.Default, new Emgu.CV.Structure.MCvScalar(1));
+					var outputImg = bitmap.ToImage<Emgu.CV.Structure.Bgr, byte>();
+					outputImg.SetZero();
+					//Emgu.CV.CvInvoke.Imshow("Emgu Window", inputImg);
+					//var outputImgPtr = CvInvoke.cvCreateImage(new System.Drawing.Size(inputImg.Width, inputImg.Height), Emgu.CV.CvEnum.IplDepth.IplDepth32S, inputImg.NumberOfChannels);
+					//var outputImg = Emgu.CV.Image<Emgu.CV.Structure.Bgr, byte>.FromIplImagePtr(outputImgPtr);
+					//Emgu.CV.CvInvoke.Imshow("Emgu Window", outputImg);
+
+					Emgu.CV.BackgroundSubtractorExtension.Apply(backSubDepth, inputImg, outputImg, -1);
+					Emgu.CV.CvInvoke.Imshow("Emgu Window", outputImg);
+					//var outputBitmap = outputImg.AsBitmap<Emgu.CV.Structure.Bgr, byte>();
+					//var bitmapSource = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(outputBitmap.GetHbitmap(), IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+					//var writeableBitmap = new WriteableBitmap(bitmapSource);
+					//colorBitmap = writeableBitmap;
+					// Write the pixel data into our bitmap
+					this.colorBitmap.WritePixels(
+						new Int32Rect(0, 0, this.colorBitmap.PixelWidth, this.colorBitmap.PixelHeight),
+						this.colorPixels,
+						this.colorBitmap.PixelWidth * sizeof(int),
+						0);
+				}
 			}
-			else if (centreHip.Position.Z - leftHand.Position.Z > 0.3)
+		}
+
+		private System.Drawing.Bitmap BitmapFromWriteableBitmap(WriteableBitmap writeBmp)
+		{
+			System.Drawing.Bitmap bmp;
+			using (MemoryStream outStream = new MemoryStream())
 			{
-				LeftRaised.Text = "Raised";
+				BitmapEncoder enc = new BmpBitmapEncoder();
+				enc.Frames.Add(BitmapFrame.Create((BitmapSource)writeBmp));
+				enc.Save(outStream);
+				bmp = new System.Drawing.Bitmap(outStream);
 			}
-			else
-			{
-				LeftRaised.Text = "Lowered";
-				RightRaised.Text = "Lowered";
-			}
+			return bmp;
 		}
 	}
 }
